3a97fef354b6d529976fa9067333f661b04b6741
NikolasAlencar <nikolasalencarrs73@gmail.com>
2024-05-22 20:37:16 -0300

    feat: implementa header

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,8 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledHeader = styled.header`
   background-color: white;
+  margin: 10px;
+  max-width: 220px;
   color: black;
-  padding: 20px;
-  text-align: center;
+  padding: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: monospace;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const StyledSvg = styled.svg`
+  height: 16px;
+  width: 16px;
+  margin-right: 8px;
+  cursor: pointer;
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,16 @@
-import { StyledHeader } from './Header.style';
+import { StyledHeader, StyledSvg } from "./Header.style";
+import { ReactComponent as House } from "../../assets/house.svg";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <StyledHeader>
-      <div>My React App</div>
+      <StyledSvg>
+        <House onClick={() => navigate("")} />
+      </StyledSvg>
+      Kanban | Welcome screen
     </StyledHeader>
   );
 };
